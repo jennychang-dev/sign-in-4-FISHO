9d9c6e27f3757cd2d09cc04e6ab66ffb6e6afd0f
Jenny Chang <jennychang@jennys-macbook-pro.local>
2019-01-23 11:37:17 -0800

    Initial Commit

---
+++ Insta-FIsh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Insta-FIsh
+//
+//  Created by Jenny Chang on 23/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Insta-FIsh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Insta-FIsh
+//
+//  Created by Jenny Chang on 23/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
