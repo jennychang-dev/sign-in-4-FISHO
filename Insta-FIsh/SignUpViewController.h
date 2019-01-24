29df810652ec24ce605904fe75956867ed055bf1
Jenny Chang <jennychang@Jennys-MacBook-Pro-2.local>
2019-01-23 21:28:34 -0800

    final commit

---
+++ Insta-FIsh/SignUpViewController.h
@@ -0,0 +1,17 @@
+//
+//  SignUpViewController.h
+//  Insta-FIsh
+//
+//  Created by Jenny Chang on 23/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SignUpViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Insta-FIsh/SignUpViewController.m
@@ -0,0 +1,32 @@
+//
+//  SignUpViewController.m
+//  Insta-FIsh
+//
+//  Created by Jenny Chang on 23/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import "SignUpViewController.h"
+
+@interface SignUpViewController ()
+
+@end
+
+@implementation SignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
